7bcb24811c855eee5a0c637cada73b9da1f56f25
Alios <80349968+Alios-5@users.noreply.github.com>
2022-08-06 12:04:18 +0300

    fmod integration + added banks

---
+++ Assets/Scripts/Audio/F_BankLoad.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_BankLoad : MonoBehaviour
+{
+    public string Bank;
+    public string BankStrings;
+
+    private void Awake()
+    {
+        
+    }
+}
